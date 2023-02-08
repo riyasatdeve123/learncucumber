b3a753a1d8015619129054012c30386ba536e6a2
Riya <riyasatdeve97@gmail.com>
2023-02-08 20:37:27 +0530

    added

---
+++ src/test/java/steps/local.java
@@ -0,0 +1,16 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class local {
+
+	@Before
+	public void setup() {
+		
+	}
+	@After
+	public void Teardown() {
+		
+	}
+}
